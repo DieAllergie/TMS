16a2336c8ee9d73de1367b9a2ade27bf2d71ffb2
DieAllergie <mymilofowork@mail.ru>
2021-10-26 19:26:24 +0300

    task_5_3.py rebilding

--- task_5_3.py
+++ task_5_3.py
@@ -9,5 +9,5 @@ for i in range(n, m + 1):
 for i in range(len(list_del)):
     for j in range(i + 1, len(list_del)):
         #print(list_dict[i].values(), list_dict[j].values())
-        if int(list_del[i]) == int(list_del[j]):
+        if int(list_del[i]) == j+n and i + n == int(list_del[j]):
             print('Числа {} и {} являются являются дружественными\n'.format(i + n, j + n))
