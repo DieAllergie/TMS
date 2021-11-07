10ab9b3e240766306a377948540459caa39fa20d
DieAllergie <mymilofowork@mail.ru>
2021-11-07 16:01:57 +0300

    task_8_3

---
+++ task_8_3.py
@@ -0,0 +1,20 @@
+def sin1(x: float, eps: float) -> float:
+    zn = x
+    n = x
+    i = 3
+    while abs(n) > eps:
+        n = n * (-1) * x * x / ((i - 1) * i)
+        i += 2
+        zn += n
+    return zn
+
+
+def main():
+    eps = [0.00001, 0.01, 0.0001, 0.001, 0.1, 0.00000001]
+    x = 0.6
+    for item in eps:
+        print(sin1(x, item))
+
+
+if __name__ == '__main__':
+    main()
