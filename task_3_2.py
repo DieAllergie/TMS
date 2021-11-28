f1fc3f130e982f1f9706f9a31452a85850283708
DieAllergie <92544479+DieAllergie@users.noreply.github.com>
2021-11-28 13:13:07 +0300

    Delete task_3_2.py

--- task_3_2.py
+++
@@ -1,8 +0,0 @@
-guest = input('Введите количество гостей на свадьбе:\n')
-if int(guest) > 50:
-    print('Закажем ресторан.')
-elif int(guest) <= 50 and int(guest) >= 20:
-    print('Закажем кафе')
-else:
-    print('Останемся дома')
-
