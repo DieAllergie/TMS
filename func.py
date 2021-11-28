2c451bf7042d55eb42aee38e1ad59c5ac5dff6b2
DieAllergie <mymilofowork@mail.ru>
2021-11-28 13:22:41 +0300

    add 4 func

---
+++ func.py
@@ -0,0 +1,17 @@
+def func_add(n: float, m: float) -> float:
+    return n + m
+
+
+def func_sub(n: float, m: float) -> float:
+    return n - m
+
+
+def func_mul(n: float, m: float) -> float:
+    return n * m
+
+
+def func_div(n: float, m: float) -> float:
+    return n / m
+
+
+
