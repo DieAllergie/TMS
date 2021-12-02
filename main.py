69db7d5651c8fd38f56da7040f22935d8309ad12
DieAllergie <mymilofowork@mail.ru>
2021-12-02 13:02:09 +0300

    moving fucn run_interface to a separate file

--- main.py
+++ main.py
@@ -1,38 +1,8 @@
-from classes import Math
+from ui_func import run_interface
 
 
 def main():
-    operation = 'some'
-    while operation != 0:
-        print('1. Сложение')
-        print('2. Вычитание')
-        print('3. Умножение')
-        print('4. Деление')
-        print('0. Выход')
-        operation = int(input('Выберете операцию: '))
-        if operation == 0:
-            break
-        if operation in (1, 2, 3, 4):
-            try:
-                n = int(input('Введите первое число: '))
-            except ValueError as err:
-                print(f'Введите допусимое значение {err}!!!!')
-                continue
-            try:
-                m = int(input('Введите второе чмсло: '))
-            except ValueError as err:
-                print(f'Введите допусимое значение {err}!!!!')
-                continue
-            if operation == 1:
-                print('Результат сложения равен: ', Math(n, m).func_add())
-            elif operation == 2:
-                print('Результат вычитания равен: ', Math(n, m).func_sub())
-            elif operation == 3:
-                print('Результат умножения равен: ', Math(n, m).func_mul())
-            elif operation == 4:
-                print('Результат деления равен: ', Math(n, m).func_div())
-        else:
-            print('Не правильная операция. Повторите выбор.')
+    run_interface()
 
 
 if __name__ == '__main__':
